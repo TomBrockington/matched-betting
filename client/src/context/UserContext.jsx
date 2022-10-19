0d8383eb3da01f00555eced412d6efb7d1734c4c
Thomas Brockington <xtombrock1989@gmail.com>
2022-10-19 14:17:46 +0100

    added user context

---
+++ client/src/context/UserContext.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { createContext, useContext, useEffect, useState } from 'react';
+
+// dunno if need export or react
+export const UserContext = React.createContext()
+
+// name of the provider this is what wraps the other, i.e <app />, files
+const UserContextProvider = ({ children }) => {
+    const [user, setUser] = useState({
+        email: 'email@email.com'
+    });
+
+    // this i what whats any pages. Anything in value is sent to that page
+    return (
+        <UserContext.Provider value={{ user }}>
+          {children}
+        </UserContext.Provider>
+      );
+}
+
+export default UserContextProvider
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Nav from '../../components/nav/Nav'
 import './style.css'
+import { UserContext } from '../../context/UserContext' 
+
 
 function Home() {
+  const user = useContext(UserContext);
+
+  console.log('user', user);
+  
   return (
     <div className='home__container'>
       <Nav />
