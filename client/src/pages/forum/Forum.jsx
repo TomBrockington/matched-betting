b2a668148caa44562ef233a1f1424b9427d5897d
Thomas Brockington <xtombrock1989@gmail.com>
2022-10-19 21:27:17 +0100

    forum layout

--- client/src/pages/forum/Forum.jsx
+++ client/src/pages/forum/Forum.jsx
@@ -1,21 +1,29 @@
-import React, { useContext } from 'react'
+import React, { useContext } from 'react';
 import Nav from '../../components/nav/Nav';
-import { UserContext } from '../../context/UserContext' 
-
+import { UserContext } from '../../context/UserContext';
+import './style.css';
 
 function Forum() {
-    const user = useContext(UserContext);
+  const user = useContext(UserContext);
+  console.log('user', user);
 
-    console.log('user', user);
   return (
-    <div className='forum__container'> 
-        <Nav />
-        <h2>Forum</h2>
-        <header className='forum__header__container'>Forum Header</header>
-        <aside className="forum_catagory__container"></aside>
-        <main className='forum__posts__container'>Posts</main>
+    <div className='forum__container'>
+      <Nav />
+      <div className='forum__content_container'>
+        <header className='forum__header__container'>
+            <h2>Forum</h2>
+        <span>Secondary header </span>
+        </header>
+
+        <div className='forum-content-container'>
+          <aside className='forum_catagory__container'>Forum categories</aside>
+          <main className='forum__posts__container'>Posts</main>
+        </div>
+
+      </div>
     </div>
-  )
+  );
 }
 
-export default Forum
+export default Forum;
