2c8f6b4406aa7cf6f570cb652e268ee735aa2571
Thomas Brockington <xtombrock1989@gmail.com>
2022-10-21 02:15:05 +0100

    jwt error for posts

--- client/src/components/posts/PostForm.jsx
+++ client/src/components/posts/PostForm.jsx
@@ -1,33 +1,85 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useState } from 'react';
+
+import { UserContext } from '../../context/UserContext';
+
+
 
 function PostForm({ postCategory }) {
+  const { user, setUser } = useContext(UserContext);
+
+  const [newPostArticle, setNewPostArticle] = useState({
+    title: '',
+    content: '',
+    category: postCategory.query,
+    ownerName: user.username
+  })
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+
+    setNewPostArticle({
+      ...newPostArticle,
+      [name]: value
+    })
+
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault(); 
+    console.log('submitting');
+
+    const { title, content, category, ownerName } = newPostArticle
+
+    const res = await fetch('http://localhost:4000/post', {
+      method: 'POST',
+      headers: {
+        'authorization': 'Bearer',
+      },
+      body: JSON.stringify(newPostArticle),
+    });
+
+    const newPostData = await res.json();
+    console.log('newPostData', newPostData);
+
+  }
+
   return (
     <section className='forum__post__container'>
-      <h3>Create a new post</h3>
+      <h3 id='form__header'>Create a new post</h3>
 
-      <form className='post__form'>
-        
+      <form className='post__form' onSubmit={handleSubmit}>
         <div className='post__title__container'>
           <label htmlFor='title'>Title</label>
-          <input type='text' name='title' />
+          <input
+            type='text'
+            name='title'
+            id='title__textbox'
+            onChange={(e) => handleChange(e)}
+          />
         </div>
 
         <div className='post__content__container'>
           <label htmlFor='content'>Content</label>
-          <textarea type='text' name='content' className='input__textarea' />
+          <textarea 
+            type='text' 
+            name='content' 
+            id='input__textarea' 
+            onChange={(e) => handleChange(e)}
+            />
         </div>
 
         <div className='post__final__container'>
-
           <div className='post__category__container'>
             <label htmlFor='category'>Category</label>
             <p>{postCategory.title}</p>
           </div>
 
           <div className='post__submit__container'>
-            <button className='post__submit__btn' type='submit'>Submit</button>
+            <button id='post__submit__btn' type='submit'>
+              Submit
+            </button>
           </div>
-
         </div>
       </form>
     </section>
