1229081008ae5854adcde8daccea34a80283fc00
Thomas Brockington <xtombrock1989@gmail.com>
2022-10-22 06:46:39 +0100

    get logged in user active

--- client/src/context/UserContext.jsx
+++ client/src/context/UserContext.jsx
@@ -2,13 +2,20 @@ import React from 'react'
 import { createContext, useContext, useEffect, useState } from 'react';
 import jwt_decode from 'jwt-decode';
 import { user } from '../utils/UserData'
+import LoggedInUser from '../utils/LoggedInUser';
 
 export const UserContext = React.createContext()
+console.log('userContext running');
 
 const initUserState = user
 
+
+
 const UserContextProvider = ({ children }) => {
-    const [user, setUser] = useState(initUserState)   
+  const [user, setUser] = useState(initUserState)   
+  
+  const decoded = LoggedInUser()
+  console.log('decodedxx', decoded);
 
     return (
         <UserContext.Provider value={{ user, setUser }}>
---
+++ client/src/utils/LoggedInUser.jsx
@@ -0,0 +1,15 @@
+import jwt_decode from 'jwt-decode';
+
+
+export default function LoggedInUser() {    
+    const loadedToken = localStorage.getItem('token');
+    console.log('loadedToken', loadedToken);
+    if (loadedToken === null || loadedToken === '') {
+        return null;
+      }
+
+      const decoded = jwt_decode(loadedToken);
+      console.log('decoded', decoded);
+
+      return decoded;
+  }
