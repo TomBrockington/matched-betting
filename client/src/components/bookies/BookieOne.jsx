674b9e122a9bb9575f624a6fa5a1f492a42d1ed9
Thomas Brockington <xtombrock1989@gmail.com>
2022-10-24 07:11:37 +0100

    added bookie components

---
+++ client/src/components/bookies/BookieOne.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function BookieOne() {
+  return (
+    <div>BookieOne</div>
+  )
+}
+
+export default BookieOne
---
+++ client/src/components/bookies/BookieTwo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function BookieTwo() {
+  return (
+    <div>BookieTwo</div>
+  )
+}
+
+export default BookieTwo
